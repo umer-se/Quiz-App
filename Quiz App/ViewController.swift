29c4d086b4c47977c333cf6d1d11eafb2d563a32
umer-se <140389967+umer-se@users.noreply.github.com>
2023-07-26 17:43:28 +0500

    Initial Commit

---
+++ Quiz App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by mac on 26/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
